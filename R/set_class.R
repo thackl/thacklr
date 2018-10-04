93bf585281ee3ae586b22abaff2eb8db7eb81453
Thomas Hackl <thackl@lim4.de>
2018-10-04 14:59:12 -0400

    better set_class() function

--- R/set_class.R
+++ R/set_class.R
@@ -1,24 +1,17 @@
 #' set class
 #'
-#' Set class with a parenthetical function.
+#' Set class of an object. Optionally append or prepend to exiting class
+#' attributes.
 #'
-#' From https://github.com/mkearney/tfse.
-#' 
 #' @param x Object to assign new class to.
-#' @param value Class value to assign to x
+#' @param class Class value to assign to x
 #' @return Object x as class value.
 #' @export
-set_class <- function(x, value) `class<-`(x, value)
-
-#' add class
-#'
-#' Add class with a parenthetical function.
-#'
-#' From https://github.com/mkearney/tfse.
-#' 
-#' @inheritParams set_class
-#' @export
-#' @rdname set_class
-add_class <- function(x, value) {
-  set_class(x, unique(c(value, class(x))))
+set_class <- function(x, class, add=c("overwrite", "prepend", "append")){
+  add = match.arg(add)
+  class <- switch(add,
+                  overwrite = class,
+                  prepend = unique(c(class, class(x))),
+                  append = unique(c(class(x), class)))
+  `class<-`(x, class)
 }
