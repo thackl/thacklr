622476b1a42c678f8181c3f9b5d91d2cf989ad10
Thomas Hackl <thackl@lim4.de>
2018-10-12 07:53:25 -0400

    a few more phylo functions

--- R/phylo.R
+++ R/phylo.R
@@ -1,16 +1,46 @@
-#' Most Recent Common Ancestor
+#' Remove missing tips
 #'
-#' Get the most recent common ancester for a set of tips. Wraps `ape::getMRCA`
-#' and also works if only a subset of tips is actually present in the tree
+#' Compare a set of tip labels to a tree, and only keep tips actually
+#' present. Through and error if no tips are present.
 #'
+#' @param tips vector of tip labels
 #' @param tree phylo object
-#' @param tips vector of tips (or nodes)
-#' @examples
-#' # "t6" is not present in tree
-#' get_mrca(ape::rtree(5), tips=c("t4","t5","t6"))
-get_mrca <- function(tree, tips){
-  tips_tree <- tips[tips %in% tree$tip.label] # MRCA needs tips present
+#' @export
+clean_tips <- function(tips, tree){
+  tips <- tips[tips %in% tree$tip.label]
   if(length(tips_tree) == 0)
     stop("none of the tips found in tree")
-  ape::getMRCA(tree, tips_tree)
+  tips
+}
+
+#' Get child nodes
+#'
+#' Get the immediate descendants for a given node.
+#'
+#' @param tree phylo object
+#' @param node parent node
+#' @export
+get_children <- function(tree, node){
+    # current node is parent
+    tree$edge[which(tree$edge[,1]==node),2]
+}
+
+#' Prune very long branches
+#'
+#' Prune tip branches that are `f` times longer than the `q`th quantile of all
+#' branch lengths in the tree.
+#'
+#' @param tree phylo object
+#' @param q quantile of all branch lengths
+#' @param f multiplicator for threshold
+#' @export
+prune_long_tip_branches <- function(tree, q=.95, f=5){
+  threshold <- quantile(tree$edge.length, q)*f
+  tips <- tree$edge[tree$edge.length > threshold, 2]
+  tips <- tips[tips <= Ntip(tree)] # only tips
+  if(length(tips) > 0){
+    write("pruning", stdout())
+    tree <- drop.tip(tree, tips)
+  }
+  tree
 }
