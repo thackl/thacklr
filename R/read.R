a153e56128f0792093f13fc8f56d615a602254a4
Thomas Hackl <thackl@lim4.de>
2019-03-15 17:06:02 -0400

    read_paf now also takes care of optional, tagged fields, and parses them into a tidy table

--- R/read.R
+++ R/read.R
@@ -1,6 +1,13 @@
-#' read a .paf file (minimap/minimap2).
+#' Read a .paf file (minimap/minimap2).
 #'
-#' Only the first 12 canonical columns. Ignores tagged extra fields.
+#' Read a minimap/minimap2 .paf file including optional tagged extra fields. The
+#' optional fields will be parsed into a tidy format, one column per tag.
+#'
+#' Because `readr::read_tsv` expects a fixed number of columns, but in .paf the
+#' number of optional fields can differ among records, this `read_paf` tries to
+#' read at least as many columns as the longest record has (`max_tags`). The
+#' resulting warnings for each record with fewer fields of the form "32 columns
+#' expected, only 22 seen" should thus be ignored.
 #'
 #' From the minimap2 manual
 #' 
@@ -39,20 +46,64 @@
 #' | dv |  f   | Approximate per-base sequence divergence              |
 #' +----+------+-------------------------------------------------------+
 #'
+#' From https://samtools.github.io/hts-specs/SAMtags.pdf
+#' type may be one of A (character), B (general array), f (real number),
+#' H (hexadecimal array), i (integer), or Z (string).
+#' 
 #' @inheritParams readr::read_tsv
 #' @importFrom readr read_tsv
+#' @param max_tags maximum number of optional fields to include
 #' @export
 #' @return tibble
-read_paf <- function(file, ...){
-  col_names <- c(
-    "query_name", "query_length", "query_start", "query_end",
-    "strand",
-    "target_name", "target_length", "target_start", "target_end",
-    "map_match", "map_length", "map_quality", "tags");
-
-  # dirty hack to compress tags into one space-sep column
-  read_tsv(
-    pipe(str_glue("bash -c \"paste <(cut -f1-12 {file}) <(cut -f13- {file} | tr '\t' ' ')\"")),
-    col_names=col_names, ...)
+read_paf <- function (file, max_tags = 20){
+  col_names <- c("query_name", "query_length", "query_start", 
+                 "query_end", "strand", "target_name", "target_length", 
+                 "target_start", "target_end", "map_match", "map_length", 
+                 "map_quality")
+  if(max_tags > 0){
+    col_names <- c(col_names, paste0("tag_", seq_len(max_tags)))
+  }
+  read_tsv(file, col_names = col_names, ...) %>%
+    tidy_paf_tags
 }
 
+tidy_paf_tags <- function(.data){
+  tag_df <- tibble(.rows=nrow(.data))
+  tag_types <- c()
+  seen_empty_tag_col <- FALSE
+  
+  for (x in select(.data, starts_with("tag_"))){
+    tag_mx <- str_split(x, ":", 3, simplify=T)
+    tag_mx_nr <- na.omit(unique(tag_mx[,1:2]))
+    if(nrow(tag_mx_nr) == 0){
+      seen_empty_tag_col <- TRUE
+      break; # empty col -> seen all tags
+    }
+    tags <- tag_mx_nr[,1]
+    tag_type <- tag_mx_nr[,2]
+    names(tag_type) <- tags
+    # add to global tag_type vec
+    tag_types <- c(tag_types, tag_type)
+    tag_types <- tag_types[unique(names(tag_types))]
+    # sort tag values into tidy tag tibble
+    for (tag in tags){
+      if(!has_name(tag_df, tag)){ # init tag
+        tag_df[[tag]] <- NA
+      }
+      tag_idx <- tag_mx[,1] %in% tag
+      tag_df[[tag]][tag_idx] <- tag_mx[tag_idx,3]
+    }
+  }
+
+  tag_df <- tag_df %>%
+    mutate_at(names(tag_types)[tag_types == "i"], as.integer) %>%
+    mutate_at(names(tag_types)[tag_types == "f"], as.numeric)
+
+  if(!seen_empty_tag_col)
+rlang::warn("Found tags in max_tags column, you should increase max_tags to ensure all tags for all entries got included")
+    
+  rlang::inform(
+        str_glue("Read and tidied up a .paf file with {n_tags} optional tag fields:\n{s_tags}", s_tags = toString(names(tag_types)), n_tags = length(tag_types)))
+
+  bind_cols(select(.data, -starts_with("tag_")), tag_df)
+}
