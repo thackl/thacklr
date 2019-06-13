1a3c47f5fe9b97c273f595310d30ad32512a5fe8
Thomas Hackl <thackl@lim4.de>
2019-06-13 12:19:32 -0400

    read bed files

---
+++ R/read_bed.R
@@ -0,0 +1,13 @@
+#' Read a .bed file
+#'
+#' @inheritParams readr::read_tsv
+#' @importFrom readr read_tsv
+#' @export
+#' @return tibble
+read_bed <- function (file){
+  col_names <- c("contig_id", "start", "end", "name", "score", "strand")
+  bed <- read_tsv(file, comment = "#", col_names=FALSE)
+  i <- seq_len(min(ncol(bed), length(col_names)))
+  names(bed)[i] <- col_names[i]
+  bed
+}
