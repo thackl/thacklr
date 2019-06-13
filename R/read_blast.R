2735c37261c067f8356341d1da3c43aa9f4aa00c
Thomas Hackl <thackl@lim4.de>
2019-06-13 12:18:59 -0400

    change blast comment char from "#" to "# " to prevent "#" in IDs to cause problems

--- R/read_blast.R
+++ R/read_blast.R
@@ -3,6 +3,10 @@
 #' Read a BLAST output in `-outfmt 6/7` format (o6/o7). If BLAST was run
 #' with custom column specifiers, those have to be passed to the call.
 #'
+#' Uses "# " to identify and ignore comment lines in o7. NOTE that any line
+#' containing this string anywhere, not limited to the line start will be
+#' trimmed after the "# ".
+#'
 #' @inheritParams readr::read_tsv
 #' @importFrom readr read_tsv
 #' @param col_spec BLAST custom column specifiers
@@ -12,5 +16,5 @@ read_blast <- function (file, col_spec="std"){
   col_std <- 'qaccver saccver pident length mismatch gapopen qstart qend sstart send evalue bitscore'
   if(col_spec == "std") col_spec <- col_std
   col_names <- strsplit(col_std, " ", TRUE)[[1]]
-  read_tsv(file, col_names = col_names, comment = "#")
+  read_tsv(file, col_names = col_names, comment = "# ")
 }
