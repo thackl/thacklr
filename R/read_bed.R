beede983c0fd5d2bb1c0bed0ec7036b07501bef4
Thomas Hackl <thackl@lim4.de>
2020-10-03 09:52:53 +0200

    contig_id to seq_id and print spec

--- R/read_bed.R
+++ R/read_bed.R
@@ -4,10 +4,10 @@
 #' @importFrom readr read_tsv
 #' @export
 #' @return tibble
-read_bed <- function (file){
-  col_names <- c("contig_id", "start", "end", "name", "score", "strand")
+read_bed <- function (file, col_names = c("seq_id", "start", "end", "name", "score", "strand")){
   bed <- read_tsv(file, comment = "#", col_names=FALSE)
   i <- seq_len(min(ncol(bed), length(col_names)))
   names(bed)[i] <- col_names[i]
+  print(glimpse(bed))
   bed
 }
