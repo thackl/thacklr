2ff883e648a0f8423df757ac80672ac2c809ca47
Thomas Hackl <thackl@lim4.de>
2021-02-01 10:25:21 +0100

    fix session_info_diff and add some doc

--- R/session_info_diff.R
+++ R/session_info_diff.R
@@ -2,7 +2,8 @@
 session_items <- function(x = sessionInfo()){
   if(inherits(x, "sessionInfo"))
     x <- capture.output(x)
-  x %>% str_split("\\s+") %>% unlist %>% discard(str_detect, "^\\[")
+  x %>% str_split("\\s+") %>% unlist %>% discard(str_detect, "^\\[") %>%
+    discard(~.x == "")
 }
 
 #' Compare sessionInfo()s
@@ -10,15 +11,17 @@ session_items <- function(x = sessionInfo()){
 #' Compare a sessionInfo string for example from a github issue to the current
 #' session and show the differences. Just copy the query session info from the
 #' issue post into the clipboard and call `session_info_diff` directly.
-#' 
+#'
 #' @export
 session_info_diff <- function(x=readLines("clipboard"), y=sessionInfo()){
-
+  y <- sessionInfo()
+  x <- si
   x <- session_items(x)
   y <- session_items(y)
   dx <- setdiff(x,y)
-  x <- tibble(x=intersect(x, dx)) %>% mutate(item=str_remove(x, "_[-.\\d]+$"))
   dy <- setdiff(y,x)
-  y <- tibble(y=intersect(y, dy)) %>% mutate(item=str_remove(y, "_[-.\\d]+$"))
-  left_join(x, y) %>% relocate(item) %>% arrange(item)
+  x <- tibble(x=dx) %>% mutate(item=str_remove(x, "_[-.\\d]+$"))
+  y <- tibble(y=dy) %>% mutate(item=str_remove(y, "_[-.\\d]+$"))
+  full_join(x, y) %>% relocate(item) %>% arrange(item) %>%
+    print(n=50)
 }
